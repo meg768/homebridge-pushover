721dc4cda01b7c6f5ac5ae13fdad25593036efb6
meg768 <meg768@users.noreply.github.com>
2019-12-09 18:36:44 +0100

    -

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 "use strict";
 
 var Path = require('path');
+var isString = require('yow/isString');
 
 module.exports = function(homebridge) {
 
-    // Load .env
-    require('dotenv').config({path: Path.join(process.env.HOME, '.homebridge/.env')});
+    // Load .env if HOME defined
+    if (isString(process.env.HOME))
+        require('dotenv').config({path: Path.join(process.env.HOME, '.homebridge/.env')});
 
-    console.log('************************************************');
     homebridge.registerPlatform('homebridge-pushover', 'Pushover', require('./src/platform.js'));
 };
