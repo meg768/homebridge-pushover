9428d482fa42b63401038c6b3bd6a63e2c19648e
meg768 <meg768@users.noreply.github.com>
2019-12-09 18:47:35 +0100

    -

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ var isString = require('yow/isString');
 module.exports = function(homebridge) {
 
     // Load .env if HOME defined
-    if (isString(process.env.HOME))
+    if (typeof process.env.HOME === 'string')
         require('dotenv').config({path: Path.join(process.env.HOME, '.homebridge/.env')});
 
     homebridge.registerPlatform('homebridge-pushover', 'Pushover', require('./src/platform.js'));
