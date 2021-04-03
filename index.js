d9f3aeb10f0c5ef29ee01323f7cc2e481b5fb939
meg768 <meg768@users.noreply.github.com>
2021-04-03 18:55:49 +0200

    -

--- index.js
+++ index.js
@@ -6,11 +6,8 @@ module.exports = function(homebridge) {
 
     var path = homebridge.user.storagePath();
 
-//    if (typeof process.env.HOME === 'string')
-//        require('dotenv').config({path: Path.join(process.env.HOME, '.homebridge/.env')});
-    
     if (path === 'string')
         require('dotenv').config({path: Path.join(path, '.env')});
     
-        homebridge.registerPlatform('homebridge-pushover', 'Pushover', require('./src/platform.js'));
+    homebridge.registerPlatform('homebridge-pushover', 'Pushover', require('./src/platform.js'));
 };
--- src/platform.js
+++ src/platform.js
@@ -1,70 +1,65 @@
-"use strict";
+'use strict';
 
 var Pushover = require('pushover-notifications');
 var Switch = require('./switch.js');
 var MasterSwitch = require('./master-switch.js');
 
 module.exports = class Platform {
+	constructor(log, config, homebridge) {
+		this.config = config;
+		this.log = log;
+		this.homebridge = homebridge;
+		this.enabled = true;
 
-    constructor(log, config, homebridge) {
-        this.config = config;
-        this.log = log;
-        this.homebridge = homebridge;
-        this.enabled = true;
+		if (process.env.PUSHOVER_USER && process.env.PUSHOVER_TOKEN) {
+			this.log('Using Pushover credentials from .env');
 
-        if (process.env.PUSHOVER_USER && process.env.PUSHOVER_TOKEN) {
-            this.log('Using Pushover credentials from .env');
+			config.pushover = {
+				user: process.env.PUSHOVER_USER,
+				token: process.env.PUSHOVER_TOKEN,
+			};
+		}
+	}
 
-            config.pushover = {
-                user: process.env.PUSHOVER_USER,
-                token: process.env.PUSHOVER_TOKEN
-            };
-        }
+	pushover(payload) {
+		return new Promise((resolve, reject) => {
+			try {
+				if (typeof payload == 'string')
+					payload = {message: payload, priority: 0};
 
-    }
+				if (!this.config.pushover) 
+					throw new Error('You must configure Pushover credentials.');
 
-    pushover(message) {
-        return new Promise((resolve, reject) => {
-            try {
-                if (!this.config.pushover)
-                    throw new Error('You must configure Pushover credentials.');
+				if (!this.config.pushover.user) 
+					throw new Error('You must configure Pushover user.');
 
-                if (!this.config.pushover.user)
-                    throw new Error('You must configure Pushover user.');
+				if (!this.config.pushover.token) 
+					throw new Error('You must configure Pushover token.');
 
-                if (!this.config.pushover.token)
-                    throw new Error('You must configure Pushover token.');
+				var push = new Pushover(this.config.pushover);
 
-                var push = new Pushover(this.config.pushover);
+				this.log('Sending Pushover payload:', payload);
 
-                this.log('Sending message:', message);
+				push.send(payload, (error, result) => {
+					if (error) reject(error);
+					else resolve();
+				});
+			} catch (error) {
+				reject(error);
+			}
+		});
+	}
 
-                push.send({priority:0, message:message}, (error, result) => {
-                    if (error)
-                        reject(error);
-                    else
-                        resolve();
-                });
-            }
-            catch (error) {
-                reject(error);
-            }
+	accessories(callback) {
+		var accessories = [];
 
-        });
-    }
+		accessories.push(new Switch(this, {name: this.config.name, message: this.config.name, type: 'masterSwitch'}));
 
-    
+		this.config.messages.forEach((item) => {
+			accessories.push(new Switch(this, item));
+		});
 
-    accessories(callback) {
-        var accessories = [];
+		callback(accessories);
+	}
+};
 
-        accessories.push((new Switch(this, {name:this.config.name, message:this.config.name, type:'masterSwitch'})));
-
-        this.config.messages.forEach((item) => {
-            accessories.push(new Switch(this, item));
-        });
-
-        callback(accessories);
-
-    }
-}
